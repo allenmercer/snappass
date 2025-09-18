0c025b3acdbb2dbda15bfb3d75eeaaec20710129
allen.mercer <allen.mercer@ankura.com>
2025-09-18 09:20:10 -0400

    add rg net and redis

---
+++ resource/terraform-manifests/03-resourcegroup.tf
@@ -0,0 +1,7 @@
+# Resource Group Block
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+  tags = local.tags
+}
---
+++ resource/terraform-manifests/04-networking.tf
@@ -0,0 +1,34 @@
+# VNET Block
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.project_name}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = [var.vnet_address_space]
+  tags                = local.tags
+}
+
+# App Service Subnet Block
+resource "azurerm_subnet" "app_snet" {
+  name                 = "${var.project_name}-app-snet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.vnet_subnet_prefix]
+  service_endpoints    = ["Microsoft.Web"]
+
+  delegation {
+    name = "app-service-delegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+# Private Endpoint Subnet Block
+resource "azurerm_subnet" "pe_snet" {
+  name                 = "${var.project_name}-pe-snet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.vnet_pe_subnet_prefix]
+  private_endpoint_network_policies_enabled = true
+}
