4191a1ca9c0a85e7df3473e283098cd5a5c4f572
allen.mercer <allen.mercer@ankura.com>
2025-09-22 14:50:23 -0400

    test

--- resource/terraform-manifests/05-redis.tf
+++ resource/terraform-manifests/05-redis.tf
@@ -11,3 +11,37 @@ resource "azurerm_redis_cache" "redis" {
   public_network_access_enabled = false
   tags                          = local.tags
 }
+
+# Private DNS Zone for Redis
+resource "azurerm_private_dns_zone" "redis_pdz" {
+  name                = "privatelink.redis.cache.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+# VNet link for the Private DNS Zone
+resource "azurerm_private_dns_zone_virtual_network_link" "redis_pdz_vnet_link" {
+  name                  = "${var.project_name}-redis-pdz-vnet-link"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.redis_pdz.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+# Private Endpoint for Redis Cache
+resource "azurerm_private_endpoint" "redis_pe" {
+  name                = "${var.project_name}-redis-pe"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.pe_snet.id
+  tags                = local.tags
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.redis_pdz.id]
+  }
+  private_service_connection {
+    name                           = "${var.project_name}-redis-psc"
+    private_connection_resource_id = azurerm_redis_cache.redis.id
+    is_manual_connection           = false
+    subresource_names              = ["redisCache"]
+  }
+}
